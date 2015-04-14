6810d914f1527bb44e35010c08d089ca19e94c5b
Mike Olsen <artfulhacker@echovoice.com>
2015-04-13 18:48:15 -0700

    remove reader C
    broke ass code needed some love

--- WiegandMega2560.h
+++ WiegandMega2560.h
@@ -7,14 +7,11 @@
 #include "WProgram.h"
 #endif
 
-#define D0PinA 2			// Arduino Pin 2 Hardware interrupt
-#define D1PinA 3			// Arduino Pin 3 Hardware interrupt
+#define D0PinA 19			// Arduino Pin 19 Hardware interrupt
+#define D1PinA 18			// Arduino Pin 18 Hardware interrupt
 
 #define D0PinB 21			// Arduino Pin 20 Hardware interrupt
 #define D1PinB 20			// Arduino Pin 21 Hardware interrupt
-
-#define D0PinC 19			// Arduino Pin 19 Hardware interrupt
-#define D1PinC 18			// Arduino Pin 18 Hardware interrupt
 
 
 class WIEGAND {
@@ -31,9 +28,7 @@ private:
 	static void ReadD0A();
 	static void ReadD1A();
 	static void ReadD0B();
-	static void ReadD1B();	
-	static void ReadD0C();
-	static void ReadD1C();		
+	static void ReadD1B();			
 	static bool DoWiegandConversion ();
 	static unsigned long GetCardId (unsigned long *codehigh, unsigned long *codelow, char bitlength);
 	
@@ -52,12 +47,6 @@ private:
 	static int				_bitCountB;	
 	static int				_wiegandTypeB;
 	static unsigned long	_codeB;
-	
-	static unsigned long 	_cardTempHighC;
-	static unsigned long 	_cardTempC;
-	static int				_bitCountC;	
-	static int				_wiegandTypeC;
-	static unsigned long	_codeC;
 };
 
 #endif
